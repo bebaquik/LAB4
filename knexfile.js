13637d744aae5232f3639fb8b2c2de26f6837377
bebaquik <34111320+bebaquik@users.noreply.github.com>
2018-05-07 15:21:45 -0300

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings LAB4 Database.
+
+module.exports = {
+
+  development: {
+    
+      client: 'postgresql',
+      connection: {
+      database: 'dev-lab4',
+      user:     'postgres',
+      password: '!@#123qweA'
+   },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'staging-lab4',
+      user:     'postgres',
+      password: '!@#123qweA'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'prod-lab4',
+      user:     'postgres',
+      password: '!@#123qweA'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
